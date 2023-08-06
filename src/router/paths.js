207eaa93d16dcaf4c6766d484e69f13259d687b8
mo7mad4 <mohammedreda814@gmail.com>
2023-08-07 00:17:59 +0300

    Make paths for project

---
+++ src/router/paths.js
@@ -0,0 +1,15 @@
+export const PATHS = {
+    HOME: '/',
+    LOGIN: '/login',
+    SIGNUP: '/signup',
+    PROFILE: '/profile',
+    ADMIN: {
+      ROOT: '/admin/home',
+      USERS: '/admin/users',
+      PROFILE: '/admin/profile',
+      SETTINGS: '/admin/settings'
+    },
+    ERRORS: {
+      NOT_FOUND: '404',
+    },
+  };
