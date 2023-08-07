9b40336168fa27b57bc4954b0f84c45cade1a8b9
mo7mad4 <mohammedreda814@gmail.com>
2023-08-07 08:58:54 +0300

    Adding app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import MainLayout from "./components/MainLayout";
+import HomePage from "./pages/HomePage";
+import "./index.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <MainLayout>
+        <HomePage />
+      </MainLayout>
+      {/* <MainLayout> */}
+      {/* <HomePage />   */}
+      {/* <Login /> */}
+      {/* <SignUp /> */}
+      {/* <HomePage /> */}
+      {/* <Profile /> */}
+      {/* <UserDetails /> */}
+      {/* <UserList /> */}
+      {/* <SideBar /> */}
+      {/* </MainLayout> */}
     </div>
   );
-}
+};
 
 export default App;
